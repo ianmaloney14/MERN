5b9953ab4e329ec3f48b9f206be3844f80c7da1c
Ian Maloney <85586358+ianmaloney14@users.noreply.github.com>
2022-09-04 15:32:53 -0700

    Hook Forms Assignment

---
+++ React/assignments/hook_form/src/components/Form.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+
+const Form = (props) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const createUser = (e) => {
+    e.preventDefault();
+
+    const newUser = { firstName, lastName, email, password, confirmPassword };
+    console.log("Welcome", newUser);
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={createUser}>
+        <div>
+          <label>First Name </label>
+          <input
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label>Last Name </label>
+          <input
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label>Email </label>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label>Password </label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label>Confirm Password </label>
+          <input
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          ></input>
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+      <h1>Your Form Data</h1>
+      <p>First Name: {firstName}</p>
+      <p>Last Name: {lastName}</p>
+      <p>Email: {email}</p>
+      <p>Password: {password}</p>
+      <p>Confirm Password: {password}</p>
+    </div>
+  );
+};
+
+export default Form;
