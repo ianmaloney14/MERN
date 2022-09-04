d64fc315bb17f9715f4c35d9c9b83ca5c0502e59
Ian Maloney <85586358+ianmaloney14@users.noreply.github.com>
2022-09-04 12:14:16 -0700

    updated for "Putting it Together" assignment

--- React/assignments/prop_it_up/src/components/PersonCard.js
+++ React/assignments/prop_it_up/src/components/PersonCard.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PersonCard = (props) => {
   const { firstName, lastName, age, hairColor } = props;
+  const [getAge, setGetAge] = useState(age);
   return (
     <div>
       <h2>
         {lastName}, {firstName}
       </h2>
-      <p>Age: {age}</p>
+      <p>Age: {getAge}</p>
       <p>Hair Color: {hairColor}</p>
+      <button onClick={(event) => setGetAge(getAge + 1)}>
+        Birthday Button for: {firstName} {lastName}
+      </button>
     </div>
   );
 };
