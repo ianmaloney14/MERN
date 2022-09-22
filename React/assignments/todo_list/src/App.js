142a3a48b8b3936b5f1a74ea356b684c9e0fc62f
Ian Maloney <85586358+ianmaloney14@users.noreply.github.com>
2022-09-22 16:46:56 -0700

    finish todo list assignment

--- React/assignments/todo_list/src/App.js
+++ React/assignments/todo_list/src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
 import './App.css';
 
+import Todo from "./components/Todo"
+
 function App() {
+  const [newTodo, setNewTodo] = useState("")
+  const [todos, setTodos] = useState([])
+
+  const handleNewTodoSubmit = (event) => {
+    event.preventDefault()
+
+    if (newTodo.length === 0) {
+      return;
+    }
+
+    const todoItem = {
+      text: newTodo,
+      complete: false
+    }
+
+    //setTodos and pass in new array with added todo
+    setTodos([...todos, todoItem])
+    setNewTodo("")
+  }
+
+  const handleTodoDelete = (delIdx) => {
+    const filteredTodos = todos.filter((_todo, i) => {
+      return i !== delIdx
+    })
+
+    setTodos(filteredTodos)
+  }
+
+  const handleToggleComplete = (idx) =>{
+    const updatedTodos = todos.map((todo, i) => {
+      if (idx === i) {
+        todo.complete = !todo.complete
+
+        //To avoid updating the todo directly, do this:
+        // const updatedTodo = { ...todo, complete: !todo.complete }
+        // return updatedTodo
+      }
+
+      return todo
+    });
+
+    setTodos(updatedTodos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign: "center"}}>
+      <form onSubmit={(event) => {
+        handleNewTodoSubmit(event)
+      }}>
+        <input 
+        onChange={(event) => {setNewTodo(event.target.value)
+        }} 
+        type="text"
+        value={newTodo}
+        />
+      <div>
+        <button>Add</button>
+      </div>
+      </form>
+      <hr></hr>
+
+      {todos.map((todo, i) => {
+          return (
+            <Todo 
+            key={i}
+            i={i}
+            todo={todo} 
+            handleToggleComplete={handleToggleComplete}
+            handleTodoDelete={handleTodoDelete}
+            />
+          );
+        })}
     </div>
   );
 }
-
 export default App;
